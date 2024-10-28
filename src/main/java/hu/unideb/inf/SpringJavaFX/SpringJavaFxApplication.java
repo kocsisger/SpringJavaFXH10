4989de0ff4ee51540a082c7757654ff21cc095ee
Teszt Elek <you@inf.unideb.hu>
2024-10-28 11:11:10 +0100

    Before the magic

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -1,21 +1,28 @@
 package hu.unideb.inf.SpringJavaFX;
 
 import hu.unideb.inf.SpringJavaFX.model.Person;
+import hu.unideb.inf.SpringJavaFX.model.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class SpringJavaFxApplication {
+public class SpringJavaFxApplication extends CommandLineRunner {
+
+	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		Person p = Person.builder()
 				.name("Thomas Smith")
 				.dateOfBirth(LocalDate.of(1998,12,11))
 				.build();
 		System.out.println(p);
 	}
-
 }
