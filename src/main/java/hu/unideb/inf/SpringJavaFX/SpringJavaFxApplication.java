e9d98c0fbd24c3255e2bb970398c74520338ab46
Teszt Elek <you@inf.unideb.hu>
2024-10-28 10:55:12 +0100

    POJO added

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -1,13 +1,21 @@
 package hu.unideb.inf.SpringJavaFX;
 
+import hu.unideb.inf.SpringJavaFX.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class SpringJavaFxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
+		Person p = Person.builder()
+				.name("Thomas Smith")
+				.dateOfBirth(LocalDate.of(1998,12,11))
+				.build();
+		System.out.println(p);
 	}
 
 }
