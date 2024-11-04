f89606ac7821d93ec4fd0aa79f9428836a675644
Teszt Elek <you@inf.unideb.hu>
2024-11-04 11:09:13 +0100

    Final

--- src/main/java/hu/unideb/inf/SpringJavaFX/frontend/JavaFXApp.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/frontend/JavaFXApp.java
@@ -1,6 +1,7 @@
 package hu.unideb.inf.SpringJavaFX.frontend;
 
 import hu.unideb.inf.SpringJavaFX.SpringJavaFxApplication;
+import hu.unideb.inf.SpringJavaFX.SpringManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,7 +11,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFXApp extends Application {
-    ConfigurableApplicationContext ctx;
+
+    static Manager manager = new SpringManager();
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -18,12 +24,14 @@ public class JavaFXApp extends Application {
                 .load(getClass().getResource("/fxml/MainFXML.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
-        ctx = SpringApplication.run(SpringJavaFxApplication.class);
+
+        manager.start();
+        System.out.println("javaFX -> " + manager.getPersonName());
     }
 
     @Override
     public void stop() throws Exception {
-        ctx.close();
+        manager.stop();
         super.stop();
     }
 }
--- src/main/java/hu/unideb/inf/SpringJavaFX/frontend/MainFXMLController.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/frontend/MainFXMLController.java
@@ -1,4 +1,16 @@
 package hu.unideb.inf.SpringJavaFX.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainFXMLController {
+
+    @FXML
+    private Label personName;
+
+    @FXML
+    public void loadData(ActionEvent actionEvent) {
+        personName.setText(JavaFXApp.manager.getPersonName());
+    }
 }
